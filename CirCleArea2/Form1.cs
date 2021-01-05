8c72d1c538e6c593b32e7b7584f9dccb08b30f11
racha01 <70879490+racha01@users.noreply.github.com>
2021-01-05 14:49:51 +0700

    Add files via upload

---
+++ CirCleArea2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CirCleArea2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const float PI = 3.141f;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float radius;
+            float area;
+            float circumference;
+            radius = float.Parse(textBox1.Text);
+            area = PI * (radius * radius);
+            circumference = 2 * PI * radius;  
+
+            MessageBox.Show("พื้นที่เท่ากับ :"+ area +"\nเส้นรอบรูปเท่ากับ :"+ circumference, "ผลการคำนวณ");
+            //MessageBox.Show("เส้นรอบรูปเท่ากับ" + area, "ผลการคำนวณ");
+        }
+    }
+}
---
+++ TowNumberCompare/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TowNumberCompare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdCalculate_Click_1(object sender, EventArgs e)
+        {
+            int number1, number2, number3;
+            int max = 0;
+            //int sum;
+
+            number1 = int.Parse(textNum1.Text);
+            number2 = int.Parse(textNum2.Text);
+            number3 = int.Parse(textNum3.Text);
+
+            //sum = number1 - number2;
+
+            //if (sum == 0)
+            //MessageBox.Show("ตัวเลขทั้งสองมีค่าเท่ากัน ซึ่งเท่ากับ " + number1);
+
+            /* int[] loopnum = new int[] { number1, number2, number3};
+              int i = 0;
+              int max = loopnum[0];
+            
+                while (i < 3)
+                {
+                    i++;
+                    if (max <= loopnum[i])
+                    {
+                        max = loopnum[i];
+                        MessageBox.Show("asd" + max);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                
+                } */
+
+            if (max <= number1)
+            {
+                max = number1;
+                if (max <= number2)
+                {
+                    max = number2;
+                    if (max <= number3)
+                    {
+                        max = number3;
+                        MessageBox.Show("ค่ามากสุดอยู่คือตัวเลขที่ 3 ซึ่งเท่ากลับ" + max);
+                    }
+                    else
+                    {
+                        max = number2;
+                        MessageBox.Show("ค่ามากสุดอยู่คือตัวเลขที่ 2 ซึ่งเท่ากลับ " + max);
+                    }
+                }
+                else if (max <= number3)
+                {
+                    max = number3;
+                    if (max <= number2)
+                    {
+                        max = number2;
+                        MessageBox.Show("ค่ามากสุดอยู่คือตัวเลขที่ 2 ซึ่งเท่ากลับ" + max);
+                    }
+                    else
+                    {
+                        max = number3;
+                        MessageBox.Show("ค่ามากสุดอยู่คือตัวเลขที่ 3 ซึ่งเท่ากลับ" + max);
+                    }
+                }
+                else
+                {
+                    max = number1;
+                    if (max <= number2)
+                    {
+                        max = number2;
+                        MessageBox.Show("ค่ามากสุดอยู่คือตัวเลขที่ 2 ซึ่งเท่ากลับ" + max);
+                    }
+                    else
+                    {
+                        max = number1;
+                        MessageBox.Show("ค่ามากสุดอยู่คือตัวเลขที่ 1 ซึ่งเท่ากลับ" + max);
+                    }
+                }
+                   
+
+
+            }
+            
+        }
+    }
+}
